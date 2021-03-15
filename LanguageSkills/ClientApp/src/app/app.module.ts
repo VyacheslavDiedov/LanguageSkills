4cde64e7706f8af9ee09fbcbc55738e72a7f225e
VyacheslavDiedov <diedov270@gmail.com>
2021-03-15 19:49:48 +0200

    Added UI header, languages and categoties with pagination

--- LanguageSkills/ClientApp/src/app/app.module.ts
+++ LanguageSkills/ClientApp/src/app/app.module.ts
@@ -3,29 +3,25 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import {SelectLanguageComponent} from './nav-menu/language/select-language.component';
+import {CategoryComponent} from './nav-menu/language/category/category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    SelectLanguageComponent,
+    CategoryComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      {path: '', component: SelectLanguageComponent, pathMatch: 'full'},
+      {path: 'category/:idLanguageToLearned', component: CategoryComponent },
     ])
   ],
   providers: [],
