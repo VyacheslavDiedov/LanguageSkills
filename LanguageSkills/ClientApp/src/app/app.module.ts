d40b91eed887d25342d42831703b9f7fb791c58f
VyacheslavDiedov <diedov270@gmail.com>
2021-03-21 18:13:42 +0200

    Added BreadCrumb

--- LanguageSkills/ClientApp/src/app/app.module.ts
+++ LanguageSkills/ClientApp/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -9,6 +9,7 @@ import {SelectLanguageComponent} from './nav-menu/language/select-language.compo
 import {CategoryComponent} from './nav-menu/language/category/category.component';
 import {SubCategoryComponent} from './nav-menu/language/subCategory/subCategory.component';
 import {SlideShowComponent} from './nav-menu/language/slideShow/slideShow.component';
+import {BreadcrumbComponent} from '../common/breadcrumb/breadcrumb.component';
 
 
 @NgModule({
@@ -18,19 +19,55 @@ import {SlideShowComponent} from './nav-menu/language/slideShow/slideShow.compon
     SelectLanguageComponent,
     CategoryComponent,
     SubCategoryComponent,
-    SlideShowComponent
+    SlideShowComponent,
+    BreadcrumbComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      {path: '', component: SelectLanguageComponent, pathMatch: 'full'},
-      {path: 'category/:idLanguageToLearned', component: CategoryComponent },
-      {path: 'subCategory/:idCategory/:categoryName', component: SubCategoryComponent },
-      {path: 'slideShow/:idSubCategory/:subCategoryName', component: SlideShowComponent }
-    ])
-  ],
+    {
+      path: '',
+      component: SelectLanguageComponent,
+      data:{
+        breadcrumb: 'Select language'
+      },
+      children:[
+        {
+          path: 'category/:idLanguageToLearned',
+          component: CategoryComponent,
+          data: {
+            breadcrumb: 'Category',
+          }, children: [
+            {
+                  path: 'subCategory/:idCategory/:categoryName',
+                  component: SubCategoryComponent,
+                  data: {
+                    breadcrumb: ''
+                  },
+              children: [
+                {
+                  path: 'slideShow/:idSubCategory/:subCategoryName',
+                  component: SlideShowComponent,
+                  data: {
+                    breadcrumb: ''
+                  },
+                }
+              ]
+            }
+          ]}
+      ],
+    },
+    ]),
+    //todo delete it after doing children
+    // RouterModule.forRoot([
+    //   {path: '', component: SelectLanguageComponent, pathMatch: 'full'},
+    //   {path: 'category/:idLanguageToLearned', component: CategoryComponent },
+    //   {path: 'subCategory/:idCategory/:categoryName', component: SubCategoryComponent },
+    //   {path: 'slideShow/:idSubCategory/:subCategoryName', component: SlideShowComponent }
+    // ])
+],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ LanguageSkills/ClientApp/src/common/breadcrumb/breadcrumb.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+import { IBreadCrumb } from './breadcrumb.interface';
+import { filter, distinctUntilChanged } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-breadcrumb',
+  templateUrl: './breadcrumb.component.html',
+  styleUrls: ['./breadcrumb.component.scss']
+})
+export class BreadcrumbComponent implements OnInit {
+  public breadcrumbs: IBreadCrumb[];
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+  ) {
+    this.breadcrumbs = this.buildBreadCrumb(this.activatedRoute.root);
+  }
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      distinctUntilChanged(),
+    ).subscribe(() => {
+      this.breadcrumbs = this.buildBreadCrumb(this.activatedRoute.root);
+    })
+  }
+
+  buildBreadCrumb(route: ActivatedRoute, url: string = '', breadcrumbs: IBreadCrumb[] = []): IBreadCrumb[] {
+    //If no routeConfig is avalailable we are on the root path
+    let label = route.routeConfig && route.routeConfig.data ? route.routeConfig.data.breadcrumb : '';
+    let path = route.routeConfig && route.routeConfig.data ? route.routeConfig.path : '';
+
+    const lastRoutePart = path.split('/').pop();
+    const isCategory = lastRoutePart.includes(':idLanguageToLearned');
+    const isDynamicRoute = lastRoutePart.startsWith(':');
+    if(isDynamicRoute && !!route.snapshot && !isCategory) {
+      const paramName = lastRoutePart.split(':')[1];
+      label = route.snapshot.params[paramName];
+    }
+
+
+    const routes = path.split('/');
+    const lastRoute = routes.pop();
+    const firstRoute = routes.pop();
+    if(isDynamicRoute && !!route.snapshot) {
+      const firstParamName = firstRoute.split(':')[1];
+      const lastParamName = lastRoute.split(':')[1];
+      path = firstParamName == undefined ? path.replace(lastRoute, route.snapshot.params[lastParamName])
+        : path.replace(lastRoute, route.snapshot.params[lastParamName]).replace(firstRoute, route.snapshot.params[firstParamName]);
+      }
+
+    //In the routeConfig the complete path is not available,
+    //so we rebuild it each time
+    const nextUrl = path ? `${url}/${path}` : url;
+
+    const breadcrumb: IBreadCrumb = {
+      label: label,
+      url: nextUrl,
+    };
+    // Only adding route with non-empty label
+    const newBreadcrumbs = breadcrumb.label ? [ ...breadcrumbs, breadcrumb ] : [ ...breadcrumbs];
+    if (route.firstChild) {
+      //If we are not on our current path yet,
+      //there will be more children to look after, to build our breadcumb
+      return this.buildBreadCrumb(route.firstChild, nextUrl, newBreadcrumbs);
+    }
+    return newBreadcrumbs;
+  }
+
+  backButtonHandle(): void {
+    if (this.breadcrumbs.length > 1) {
+      this.breadcrumbs.pop();
+      this.router.navigate([this.breadcrumbs[this.breadcrumbs.length-1].url])
+    }
+  }
+}
