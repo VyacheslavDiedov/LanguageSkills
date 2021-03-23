3370f5ad874e074c061f5bb565fe4b7d6c1b772e
VyacheslavDiedov <diedov270@gmail.com>
2021-03-23 17:10:50 +0200

    Adaptive the category to mobile

--- LanguageSkills/ClientApp/src/app/nav-menu/language/category/category.component.ts
+++ LanguageSkills/ClientApp/src/app/nav-menu/language/category/category.component.ts
@@ -15,32 +15,49 @@ export class CategoryWithTranslation{
 })
 export class CategoryComponent implements OnInit{
   idLanguageToLearned: number;
+  innerWidth: number;
+  categoryWithTranslations: CategoryWithTranslation[];
 
   constructor(private activateRoute: ActivatedRoute){
     this.idLanguageToLearned = activateRoute.snapshot.params['idLanguageToLearned'];
+    this.innerWidth = window.innerWidth;
   }
 
   ngOnInit() {
-    //todo make query
+    //todo it will get from back end
+    if( this.innerWidth > 650){
+    this.categoryWithTranslations = [
+        {id: 1, categoryName: 'Nature and environment', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Nature and environment'},
+        {id: 2, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
+        {id: 3, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
+        {id: 4, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
+        {id: 5, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
+        {id: 6, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
+        {id: 7, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
+        {id: 8, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
+        {id: 9, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
+        {id: 10, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
+        {id: 11, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
+        {id: 12, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
+        {id: 13, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
+        {id: 14, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
+        {id: 15, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
+      ];
+    }else{
+      this.categoryWithTranslations = [
+        {id: 1, categoryName: 'Nature and environment', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Nature and environment'},
+        {id: 2, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
+        {id: 3, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
+        {id: 4, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
+        {id: 5, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
+        {id: 6, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
+        {id: 7, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
+        {id: 8, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
+      ];
+    }
   }
 
-  public categoryWithTranslations: CategoryWithTranslation[] = [
-    {id: 1, categoryName: 'Nature and environment', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Nature and environment'},
-    {id: 2, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
-    {id: 3, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
-    {id: 4, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
-    {id: 5, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
-    {id: 6, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
-    {id: 7, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
-    {id: 8, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
-    {id: 9, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
-    {id: 10, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
-    {id: 11, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
-    {id: 12, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
-    {id: 13, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
-    {id: 14, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
-    {id: 15, categoryName: 'Animal', categoryImagePath: '../../../../assets/img/Ukraine.jpg', categoryTranslationLearnedName: 'Animal'},
-  ];
+
 
   isActive(pageNumber) {
     if (pageNumber === 2) {
